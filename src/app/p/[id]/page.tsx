3821ec8b6a666e637e928ec1727885f6fa0e2704
Ismael Martínez <hola@isma.uy>
2024-11-21 16:50:19 -0300

    update padding in main layout for improved spacing

--- src/app/p/[id]/page.tsx
+++ src/app/p/[id]/page.tsx
@@ -15,7 +15,7 @@ export default async function Page({
   if (!item) notFound();
 
   return (
-    <main className="mx-auto max-w-screen-md">
+    <main className="mx-auto max-w-screen-md px-4">
       <div className="py-20">
         <Link href="/">← back to list</Link>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { data } from "@/app/data";
 
 export default function Home() {
   return (
-    <main className="mx-auto max-w-screen-md">
-      <div id="grid" className="grid grid-cols-2 py-20 gap-10">
+    <main className="mx-auto max-w-screen-md px-5">
+      <div id="grid" className="grid grid-cols-2 py-20 gap-5 md:gap-10">
         {data.map((item) => (
           <Link
             key={item.name}
